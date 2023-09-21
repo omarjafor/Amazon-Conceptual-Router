66f0ccc5810c2b269ae534907b693165827b219d
omarjafor <omarjaforchy@gmail.com>
2023-09-21 23:49:11 +0600

    Product added from api

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -7,7 +7,7 @@ const Products = () => {
     console.log(products);
     
     return (
-        <div>
+        <div className='grid grid-cols-3 gap-5 p-5'>
             {products?.map(product => <Product key={product.id} product={product}></Product>)}
         </div>
     );
